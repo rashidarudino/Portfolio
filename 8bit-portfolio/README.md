a0d957db240ec69641a4d1fff9b695ad2c62b295
sphere-bit <z5363922@ad.unsw.edu.au>
2024-09-09 02:11:36 +1000

    Remove unused templates

---
+++ 8bit-portfolio/README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  <img src="https://github.com/TheOrcDev/8bit-portfolio/assets/7549148/349c8acd-4b21-4996-b631-5227b2eb3fc3" alt="pixel-orc"/>
+</div>
+
+## Overview
+Welcome to the 8-bit Pixely Portfolio Orcish Template! This unique developer portfolio is crafted in Next.js and styled using the nes.css library to bring a nostalgic, old-school NES look. Perfect for developers who want to showcase their projects with a retro gaming aesthetic, this template combines modern web development with a delightful 8-bit design.
+
+## 8-bit Pixely Portfolio Orcish Template
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ 8bit-portfolio/app/page.tsx
@@ -0,0 +1,141 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-10 gap-10">
+      <h1 className="text-center">
+        Are you looking to become a true web dev warrior?
+      </h1>
+
+      <div className="nes-container with-title is-centered bg-blue-200 md:w-[45rem]">
+        <p className="title">Profile</p>
+        <p>
+          My creative grows as I tutor students at Cluey Learning, and having recently 
+          graduated as a computer science bachelor, I have lots to share about technology.
+          So feel free to browse, as I share some works of mine.
+        </p>
+      </div>
+
+      <div className="nes-container with-title is-centered bg-white md:w-[45rem]">
+        <p className="title">Education</p>
+        Bachelor of <strong>Computer Science</strong> at UNSW Sydney Kensington Campus, 2024 August.
+      </div>
+
+      <div className="nes-container is-rounded bg-white">
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          width={100}
+          height={100}
+          src={"/axe-and-shield.webp"}
+          alt="Axe & Shield"
+        />
+        <p className="text-center">These are my skills</p>
+
+        {/* Frontend Development */}
+        <h2 className="text-center mt-4 mb-2">Frontend Development</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">JavaScript</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-warning">HTML/CSS</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-dark">React.js</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-success">MUI</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-error">Astro</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">Svelte</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-warning">Next.js</span>
+          </Link>
+        </div>
+
+        {/* Backend Development */}
+        <h2 className="text-center mt-4 mb-2">Backend Development</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Link href="#" className="nes-badge">
+            <span className="is-dark">Node.js</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-success">Express.js</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-error">REST API</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">MySQL</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-warning">PostgreSQL</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-dark">Apache2</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-success">Nginx</span>
+          </Link>
+        </div>
+
+        {/* Programming Languages */}
+        <h2 className="text-center mt-4 mb-2">Programming Languages</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Link href="#" className="nes-badge">
+            <span className="is-error">C++</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">Python</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-warning">Rust</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-dark">Shell</span>
+          </Link>
+        </div>
+
+        {/* Tools & Frameworks */}
+        <h2 className="text-center mt-4 mb-2">Tools & Frameworks</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Link href="#" className="nes-badge">
+            <span className="is-success">Git</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-error">Postman API</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">Jira</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-warning">CI/CD</span>
+          </Link>
+        </div>
+
+        {/* Game & App Development */}
+        <h2 className="text-center mt-4 mb-2">Game & App Development</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Link href="#" className="nes-badge">
+            <span className="is-dark">Revit</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-success">Unreal</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-error">Unity2D</span>
+          </Link>
+          <Link href="#" className="nes-badge">
+            <span className="is-primary">PowerApps</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+    </main>
+  );
+}
