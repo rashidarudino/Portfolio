85f3f8d6eafac3782f94eb738aa2a03952b27fcb
sphere-bit <z5363922@ad.unsw.edu.au>
2024-09-15 01:53:20 +1000

    Edit corner button

--- components/ui/subscribe-tea/subscribe-tea.tsx
+++ components/ui/subscribe-tea/subscribe-tea.tsx
@@ -3,16 +3,21 @@
 import Image from 'next/legacy/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import { FaArrowRight, FaPlane } from 'react-icons/fa';
+import { SiThreedotjs } from 'react-icons/si';
 
 export default function SubscribeOrc() {
   return (
-    <div className='fixed bottom-0 right-0 w-32 md:w-64 hidden sm:flex items-center'>
+    <div className='fixed bottom-0 right-0 w-32 flex items-center'>
       <Link href={'https://rashidarudino.netlify.app/'}>
+        <div className='nes-balloon from-right w-24 animate-bounce'>
+          <p className='text -ml-4'>Click</p>
+        </div>
         <Image
           src={'/revit/house.gif'}
           priority
-          width={300}
-          height={250}
+          width={150}
+          height={100}
           alt='Orc'
         />
       </Link>
